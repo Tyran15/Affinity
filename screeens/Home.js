54bcff1fe4cd9cc405c71f89cde6aea2d5cd5164
Tyran15 <mh15122006@gmail.com>
2023-10-22 18:25:48 -0300

    Adição da tela Home e estilização da tela

--- screeens/Home.js
+++ screeens/Home.js
@@ -1,12 +1,21 @@
+import { LinearGradient } from 'expo-linear-gradient';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 
-export default function App() {
+export default function Home() {
   return (
-    <View style={styles.container}>
-      <Text>Home</Text>
-      <StatusBar style="auto" />
-    </View>
+      <LinearGradient colors={['#400E57', '#004DBF']} style={styles.container}>
+        <View style={styles.caixa1}>
+          <Image source={require('../assets/Imagens/Logo.png')} style={styles.imagem}/>
+          <Text style={styles.text}>O seu parceiro está a um clique de Distância</Text>
+        </View>
+        <View style={styles.caixa2}>
+          <Text style={styles.text}>Ao tentar entrar você concorda com nossos Termos de uso e privacidade</Text>
+          <TouchableOpacity style={styles.button}>Entrar</TouchableOpacity>
+          <TouchableOpacity style={styles.button}>Cadastrar</TouchableOpacity>
+        </View>
+        <StatusBar style="auto" />
+      </LinearGradient>
   );
 }
 
@@ -16,5 +25,32 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  imagem: {
+    height: '12rem',
+    width: '100%',
+  },
+  text: {
+    color: 'white',
+    textAlign: 'center',
+    width: '20rem',
+    fontSize: 16,
+  },
+  caixa1: {
+    marginBottom: '50%',
+  },
+  caixa2: {
+    alignItems: 'center',
+  },
+  button: {
+    borderWidth: 2,
+    padding: 5,
+    width: 180,
+    marginTop: 30,
+    fontSize: 24,
+    textAlign: 'center',
+    color: 'white',
+    borderColor: 'white',
+    borderRadius: 35,
   },
 });
