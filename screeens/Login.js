b15b375dccbb98452e3b1f6fa194cdd5e13c916e
Tyran15 <mh15122006@gmail.com>
2023-10-23 15:00:32 -0300

    Estilização da tela de login

--- screeens/Login.js
+++ screeens/Login.js
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
-export default function App() {
+export default function Login() {
   return (
-    <View style={styles.container}>
-      <Text>Login</Text>
+  <LinearGradient colors={['#400E57', '#004DBF']} style={styles.container}>      
+    <View style={styles.caixa1}>
+        <Text style={styles.tittle}>Login</Text>
+        <TextInput style={styles.input} placeholder='E-mail' />
+        <TextInput style={styles.input} placeholder='Senha' />
+      </View>
+
+      <View style={styles.caixa2}>
+        <TouchableOpacity style={styles.button}>Entrar</TouchableOpacity>
+        <TouchableOpacity style={styles.link}>Esqueceu a senha?</TouchableOpacity>
+      </View>
+      
       <StatusBar style="auto" />
-    </View>
+    </LinearGradient>
   );
 }
 
@@ -16,5 +27,45 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  caixa1: {
+    marginBottom: '50%',
+    alignItems: 'center',
+  },
+  caixa2: {
+    alignItems: 'center',
+  },
+  tittle: {
+    color: 'white',
+    fontSize: 48,
+    marginBottom: '25%',
+    fontWeight: 'bold',
+  },
+  input: {
+    borderBottomColor: 'white',
+    borderBottomWidth: 2,
+    color: 'white',
+    width: 250,
+    marginBottom: '10%',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  button: {
+    borderColor: 'white',
+    borderWidth: 2,
+    color: 'white',
+    width: 180,
+    height: 44,
+    textAlign: 'center',
+    fontSize: 24,
+    justifyContent: 'center',
+    borderRadius: 35,
+    fontWeight: 'bold',
+    marginBottom: '15%',
+  },
+  link: {
+    fontSize: 24,
+    color: 'white',
+    textDecorationLine: 'underline',  
   },
 });
