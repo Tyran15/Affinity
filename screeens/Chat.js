57ab70a5a7b974f2aa7f469cf1240ff224d13ec1
Tyran15 <mh15122006@gamil.com>
2023-11-13 14:16:16 -0300

    Adição das funções a tela de chat

--- screeens/Chat.js
+++ screeens/Chat.js
@@ -1,22 +1,77 @@
-import { StyleSheet, Text, View, Image } from "react-native";
-import { StatusBar } from "expo-status-bar";
+import React from 'react';
+import { View, Text, FlatList, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 
-export default function Chat() {
+const Chat = () => {
+  const [messages, setMessages] = React.useState([]);
+  const [newMessage, setNewMessage] = React.useState('');
+
+  const handleSendMessage = () => {
+    if (newMessage.trim() === '') return;
+
+    setMessages((prevMessages) => [
+      ...prevMessages,
+      { id: messages.length + 1, text: newMessage, user: 'user' },
+    ]);
+    setNewMessage('');
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Chat</Text>
+      <FlatList
+        data={messages}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.messageContainer}>
+            <Text>{`${item.user}: ${item.text}`}</Text>
+          </View>
+        )}
+      />
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Type a message"
+          value={newMessage}
+          onChangeText={(text) => setNewMessage(text)}
+        />
+        <TouchableOpacity onPress={handleSendMessage} style={styles.sendButton}>
+          <Text style={styles.sendButtonText}>Send</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    padding: 16,
+    backgroundColor: '#fff',
   },
-  title: {
+  messageContainer: {
+    marginBottom: 10,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  input: {
+    flex: 1,
+    height: 40,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    marginRight: 10,
+    padding: 8,
+  },
+  sendButton: {
+    backgroundColor: '#007BFF',
+    borderRadius: 5,
+    padding: 10,
+  },
+  sendButtonText: {
+    color: '#fff',
     fontWeight: 'bold',
-  }
+  },
 });
+
+export default Chat;
