bc00b60ed352417b0717a64be3c91a25ef91029b
Tyran15 <mh15122006@gamil.com>
2023-11-13 15:01:28 -0300

    alteração no chat

--- screeens/Chat.js
+++ screeens/Chat.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, FlatList, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, FlatList, TextInput, TouchableOpacity, StyleSheet, Image } from 'react-native';
 
 const Chat = () => {
   const [messages, setMessages] = React.useState([]);
@@ -17,12 +17,17 @@ const Chat = () => {
 
   return (
     <View style={styles.container}>
+      <View style={styles.containerTitulo}>
+        <Image source={require('../assets/Imagens/Logo.png')} style={styles.imgLogo}/>
+        <Text style={styles.tituloChat}>Chat</Text>
+      </View>
       <FlatList
+        style={styles.conteinerFlatList}
         data={messages}
         keyExtractor={(item) => item.id.toString()}
         renderItem={({ item }) => (
           <View style={styles.messageContainer}>
-            <Text>{`${item.user}: ${item.text}`}</Text>
+            <Text style={styles.novaMessage}>{`${item.text}`}</Text>
           </View>
         )}
       />
@@ -47,9 +52,54 @@ const styles = StyleSheet.create({
     padding: 16,
     backgroundColor: '#fff',
   },
-  messageContainer: {
-    marginBottom: 10,
+  conteinerTitulo:{
+    marginTop:20,
+    margin:10,
+    width:'auto',
+    height:'auto',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
+  tituloChat:{
+    textAlign: 'center',
+    text: 'bold',
+    fontSize: 20,
+  },
+  imgVip: {
+    marginTop:20,
+    width:110,
+    height:40,
+  },
+  imgLogo: {
+    height: 90,
+    width: 90,
+  },
+
+
+
+  conteinerFlatList:{
+    paddingLeft:30,
+  },
+  messageContainer: {
+    color: 'white',
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 0,
+    backgroundColor: '#640525',
+    borderColor:'#640525',
+    padding: 5,
+    borderWidth: 2,
+    marginBottom: 10,
+    width: 'auto',
+  },
+  novaMessage:{
+    textAlign: 'right',
+    color: 'white',
+    fontSize: 17,
+    width: 'auto',
+  },
+
   inputContainer: {
     flexDirection: 'row',
     alignItems: 'center',
